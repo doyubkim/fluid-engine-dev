cbe58280f7f3d3c3b5e8126d2ce485006f689ce9
Doyub Kim <doyubkim@gmail.com>
2016-08-14 23:38:44 -0700

    Documenting grid-based pressure solvers

--- include/jet/grid_single_phase_pressure_solver3.h
+++ include/jet/grid_single_phase_pressure_solver3.h
@@ -10,12 +10,48 @@
 
 namespace jet {
 
+//!
+//! \brief 3-D single-phase pressure solver.
+//!
+//! This class implements 3-D single-phase pressure solver. This solver encodes
+//! the boundaries like Lego blocks -- if a grid cell center is inside or
+//! outside the boundaries, it is either marked as occupied or not.
+//! In addition, this class solves single-phase flow, solving the pressure for
+//! selected fluid region only and treat other area as an atmosphere region.
+//! Thus, the pressure outside the fluid will be set to a constant value and
+//! velocity field won't be altered. This solver also computes the fluid
+//! boundary in block-like manner; If a grid cell is inside or outside the
+//! fluid, it is marked as either fluid or atmosphere. Thus, this solver in
+//! general, does not compute subgrid structure.
+//!
 class GridSinglePhasePressureSolver3 : public GridPressureSolver3 {
  public:
+    //! Default constructor.
     GridSinglePhasePressureSolver3();
 
+    //! Default destructor.
     virtual ~GridSinglePhasePressureSolver3();
 
+    //!
+    //! \brief Solves the pressure term and apply it to the velocity field.
+    //!
+    //! This function takes input velocity field and outputs pressure-applied
+    //! velocity field. It also accepts extra arguments such as \p boundarySdf
+    //! and \p fluidSdf that represent signed-distance representation of the
+    //! boundary and fluid area. The negative region of \p boundarySdf means
+    //! it is occupied by solid object. Also, the positive / negative area of
+    //! the \p fluidSdf means it is occupied by fluid / atmosphere. If not
+    //! specified, constant scalar field with kMaxD will be used for
+    //! \p boundarySdf meaning that no boundary at all. Similarly, a constant
+    //! field with -kMaxD will be used for \p fluidSdf which means it's fully
+    //! occupied with fluid without any atmosphere.
+    //!
+    //! \param[in]    input                 The input velocity field.
+    //! \param[in]    timeIntervalInSeconds The time interval for the sim.
+    //! \param[inout] output                The output velocity field.
+    //! \param[in]    boundarySdf           The SDF of the boundary.
+    //! \param[in]    fluidSdf              The SDF of the fluid/atmosphere.
+    //!
     void solve(
         const FaceCenteredGrid3& input,
         double timeIntervalInSeconds,
@@ -25,14 +61,26 @@ class GridSinglePhasePressureSolver3 : public GridPressureSolver3 {
         const ScalarField3& fluidSdf
             = ConstantScalarField3(-kMaxD)) override;
 
+    //!
+    //! \brief Returns the best boundary condition solver for this solver.
+    //!
+    //! This function returns the best boundary condition solver that works well
+    //! with this pressure solver. Depending on the pressure solver
+    //! implementation, different boundary condition solver might be used. For
+    //! this particular class, an instance of
+    //! GridBlockedBoundaryConditionSolver2 will be returned since this pressure
+    //! solver encodes boundaries like pixelated Lego blocks.
+    //!
     GridBoundaryConditionSolver3Ptr
         suggestedBoundaryConditionSolver() const override;
 
+    //! Sets the linear system solver.
     void setLinearSystemSolver(const FdmLinearSystemSolver3Ptr& solver);
 
+    //! Returns the pressure field.
     const FdmVector3& pressure() const;
 
- protected:
+ private:
     FdmLinearSystem3 _system;
     FdmLinearSystemSolver3Ptr _systemSolver;
     Array3<char> _markers;
