a27f384a248832980c379551e66191bb2429034a
Doyub Kim <doyubkim@gmail.com>
2016-11-27 11:07:12 -0800

    Fix the order issue for the 2-D grid fluid solver as well

--- src/jet/grid_fluid_solver2.cpp
+++ src/jet/grid_fluid_solver2.cpp
@@ -158,6 +158,11 @@ void GridFluidSolver2::onAdvanceTimeStep(double timeIntervalInSeconds) {
     beginAdvanceTimeStep(timeIntervalInSeconds);
 
     Timer timer;
+    computeAdvection(timeIntervalInSeconds);
+    JET_INFO << "Computing advection force took "
+             << timer.durationInSeconds() << " seconds";
+
+    timer.reset();
     computeExternalForces(timeIntervalInSeconds);
     JET_INFO << "Computing external force took "
              << timer.durationInSeconds() << " seconds";
@@ -170,11 +175,6 @@ void GridFluidSolver2::onAdvanceTimeStep(double timeIntervalInSeconds) {
     timer.reset();
     computePressure(timeIntervalInSeconds);
     JET_INFO << "Computing pressure force took "
-             << timer.durationInSeconds() << " seconds";
-
-    timer.reset();
-    computeAdvection(timeIntervalInSeconds);
-    JET_INFO << "Computing advection force took "
              << timer.durationInSeconds() << " seconds";
 
     endAdvanceTimeStep(timeIntervalInSeconds);
