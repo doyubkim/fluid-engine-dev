9da8543c2aa8292bfc8bf9c0ab4fd2c41c61d1c6
Doyub Kim <doyubkim@gmail.com>
2016-11-30 22:59:12 -0800

    Adding documentation and tests for ray and samplers

--- include/jet/ray2.h
+++ include/jet/ray2.h
@@ -8,6 +8,11 @@
 
 namespace jet {
 
+//!
+//! \brief      Class for 2-D ray.
+//!
+//! \tparam     T     The value type.
+//!
 template <typename T>
 class Ray<T, 2> final {
  public:
@@ -15,15 +20,22 @@ class Ray<T, 2> final {
         std::is_floating_point<T>::value,
         "Ray only can be instantiated with floating point types");
 
+    //! The origin of the ray.
     Vector2<T> origin;
+
+    //! The direction of the ray.
     Vector2<T> direction;
 
+    //! Constructs an empty ray that points (1, 0) from (0, 0).
     Ray();
 
+    //! Constructs a ray with given origin and riection.
     Ray(const Vector2<T>& newOrigin, const Vector2<T>& newDirection);
 
+    //! Copy constructor.
     Ray(const Ray& other);
 
+    //! Returns a point on the ray at distance \p t.
     Vector2<T> pointAt(T t) const;
 };
 
---
+++ src/tests/unit_tests/ray3_tests.cpp
@@ -0,0 +1,25 @@
+// Copyright (c) 2016 Doyub Kim
+
+#include <jet/ray3.h>
+#include <gtest/gtest.h>
+
+using namespace jet;
+
+TEST(Ray3, Constructors) {
+    Ray3D ray;
+    EXPECT_EQ(Vector3D(), ray.origin);
+    EXPECT_EQ(Vector3D(1, 0, 0), ray.direction);
+
+    Ray3D ray2({1, 2, 3}, {4, 5, 6});
+    EXPECT_EQ(Vector3D(1, 2, 3), ray2.origin);
+    EXPECT_EQ(Vector3D(4, 5, 6).normalized(), ray2.direction);
+
+    Ray3D ray3(ray2);
+    EXPECT_EQ(Vector3D(1, 2, 3), ray3.origin);
+    EXPECT_EQ(Vector3D(4, 5, 6).normalized(), ray3.direction);
+}
+
+TEST(Ray3, PointAt) {
+    Ray3D ray;
+    EXPECT_EQ(Vector3D(4.5, 0.0, 0.0), ray.pointAt(4.5));
+}
