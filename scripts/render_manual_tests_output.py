7a2ddd9e75291dc95c766a071a51c2a77ec33071
doyubkim <doyubkim@gmail.com>
2016-12-03 22:23:06 +0000

    [ci skip] Workaround for rendering script issues

--- scripts/render_manual_tests_output.py
+++ scripts/render_manual_tests_output.py
@@ -12,22 +12,28 @@ if __name__ == '__main__':
     filenames.sort()
 
     for filename in filenames:
-        tags = render.parse_tags(filename)
-        # Pick the first component only
-        if render.Y_TAG in tags or render.Z_TAG in tags:
-            continue
-        # Pick the first frame only
-        if render.is_animation(tags) and '0000' not in tags:
-            continue
-        if render.POINT2_TAG in tags:
-            render.render_point2(filename)
-        elif render.POINT3_TAG in tags:
-            render.render_point3(filename)
-        elif render.LINE2_TAG in tags:
-            render.render_line2(filename)
-        elif render.LINE3_TAG in tags:
-            render.render_line3(filename)
-        elif render.GRID2_TAG in tags:
-            render.render_grid2(filename)
-        elif render.GRID3_TAG in tags:
-            render.render_grid3(filename)
+        try:
+            tags = render.parse_tags(filename)
+            # Pick the first component only
+            if render.Y_TAG in tags or render.Z_TAG in tags:
+                continue
+            # Pick the first frame only
+            if render.is_animation(tags) and '0000' not in tags:
+                continue
+            if render.POINT2_TAG in tags:
+                render.render_point2(filename)
+            elif render.POINT3_TAG in tags:
+                render.render_point3(filename)
+            elif render.LINE2_TAG in tags:
+                render.render_line2(filename)
+            elif render.LINE3_TAG in tags:
+                render.render_line3(filename)
+            elif render.GRID2_TAG in tags:
+                render.render_grid2(filename)
+            elif render.GRID3_TAG in tags:
+                render.render_grid3(filename)
+        except Exception as e:
+            print 'Failed to render', filename
+            print 'Why?'
+            print e
+
