e75d8d2b780f73b70f05ee65ec23548cb4888ac4
Doyub Kim <doyubkim@gmail.com>
2017-03-28 00:02:36 -0700

    Ray-triangle intersection bug fix

--- src/tests/unit_tests/triangle3_tests.cpp
+++ src/tests/unit_tests/triangle3_tests.cpp
@@ -4,8 +4,9 @@
 // personal capacity and am not conveying any rights to any intellectual
 // property of any third parties.
 
+#include <unit_tests_utils.h>
+
 #include <jet/triangle3.h>
-#include <gtest/gtest.h>
 
 using namespace jet;
 
@@ -21,21 +22,12 @@ TEST(Triangle3, Constructors) {
         }
     }
 
-    std::array<Vector3D, 3> points = {{
-        Vector3D(1, 2, 3),
-        Vector3D(4, 5, 6),
-        Vector3D(7, 8, 9)
-    }};
-    std::array<Vector3D, 3> normals = {{
-        Vector3D(1, 0, 0),
-        Vector3D(0, 1, 0),
-        Vector3D(0, 0, 1)
-    }};
-    std::array<Vector2D, 3> uvs = {{
-        Vector2D(1, 0),
-        Vector2D(0, 1),
-        Vector2D(0.5, 0.5)
-    }};
+    std::array<Vector3D, 3> points = {
+            {Vector3D(1, 2, 3), Vector3D(4, 5, 6), Vector3D(7, 8, 9)}};
+    std::array<Vector3D, 3> normals = {
+            {Vector3D(1, 0, 0), Vector3D(0, 1, 0), Vector3D(0, 0, 1)}};
+    std::array<Vector2D, 3> uvs = {
+            {Vector2D(1, 0), Vector2D(0, 1), Vector2D(0.5, 0.5)}};
 
     Triangle3 tri2(points, normals, uvs);
     for (int i = 0; i < 3; ++i) {
@@ -60,28 +52,61 @@ TEST(Triangle3, Constructors) {
     }
 }
 
+TEST(Triangle3, BasicGetters) {
+    Triangle3 tri;
+    tri.points = {{Vector3D(0, 0, -1), Vector3D(1, 0, -1), Vector3D(0, 1, -1)}};
+
+    EXPECT_DOUBLE_EQ(0.5, tri.area());
+
+    double b0, b1, b2;
+    tri.getBarycentricCoords(Vector3D(0.5, 0.5, -1), &b0, &b1, &b2);
+    EXPECT_DOUBLE_EQ(0.0, b0);
+    EXPECT_DOUBLE_EQ(0.5, b1);
+    EXPECT_DOUBLE_EQ(0.5, b2);
+
+    Vector3D n = tri.faceNormal();
+    EXPECT_VECTOR3_EQ(Vector3D(0, 0, 1), n);
+}
+
+TEST(Triangle3, SurfaceGetters) {
+    Triangle3 tri;
+    tri.points = {{Vector3D(0, 0, -1), Vector3D(1, 0, -1), Vector3D(0, 1, -1)}};
+    tri.normals = {{Vector3D(1, 0, 0), Vector3D(0, 1, 0), Vector3D(0, 0, 1)}};
+    Vector3D cp1 = tri.closestPoint({0.4, 0.4, 3.0});
+    EXPECT_VECTOR3_EQ(Vector3D(0.4, 0.4, -1), cp1);
+
+    Vector3D cp2 = tri.closestPoint({-3.0, -3.0, 0.0});
+    EXPECT_VECTOR3_EQ(Vector3D(0, 0, -1), cp2);
+
+    bool ints1 = tri.intersects(Ray3D({0.4, 0.4, -5.0}, {0, 0, 1}));
+    EXPECT_TRUE(ints1);
+
+    bool ints2 = tri.intersects(Ray3D({-1, 2, 3}, {0, 0, -1}));
+    EXPECT_FALSE(ints2);
+
+    bool ints3 = tri.intersects(Ray3D({1, 1, 0}, {0, 0, -1}));
+    EXPECT_FALSE(ints3);
+
+    auto cints1 = tri.closestIntersection(Ray3D({0.4, 0.4, -5.0}, {0, 0, 1}));
+    EXPECT_TRUE(cints1.isIntersecting);
+    EXPECT_VECTOR3_EQ(Vector3D(0.4, 0.4, -1), cints1.point);
+    EXPECT_DOUBLE_EQ(4.0, cints1.t);
+    EXPECT_VECTOR3_EQ(Vector3D(1, 2, 2).normalized(), cints1.normal);
+}
+
 TEST(Triangle3, Builder) {
-    std::array<Vector3D, 3> points = {{
-        Vector3D(1, 2, 3),
-        Vector3D(4, 5, 6),
-        Vector3D(7, 8, 9)
-    }};
-    std::array<Vector3D, 3> normals = {{
-        Vector3D(1, 0, 0),
-        Vector3D(0, 1, 0),
-        Vector3D(0, 0, 1)
-    }};
-    std::array<Vector2D, 3> uvs = {{
-        Vector2D(1, 0),
-        Vector2D(0, 1),
-        Vector2D(0.5, 0.5)
-    }};
+    std::array<Vector3D, 3> points = {
+            {Vector3D(1, 2, 3), Vector3D(4, 5, 6), Vector3D(7, 8, 9)}};
+    std::array<Vector3D, 3> normals = {
+            {Vector3D(1, 0, 0), Vector3D(0, 1, 0), Vector3D(0, 0, 1)}};
+    std::array<Vector2D, 3> uvs = {
+            {Vector2D(1, 0), Vector2D(0, 1), Vector2D(0.5, 0.5)}};
 
     Triangle3 tri = Triangle3::builder()
-        .withPoints(points)
-        .withNormals(normals)
-        .withUvs(uvs)
-        .build();
+            .withPoints(points)
+            .withNormals(normals)
+            .withUvs(uvs)
+            .build();
 
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
