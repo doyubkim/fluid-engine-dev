29578850ebc16b67485ce7be2df04d39ed8baf5a
Doyub Kim <doyubkim@gmail.com>
2017-11-06 22:11:57 -0800

    Initial CUDA setup

---
+++ include/jet/cuda_test.h
@@ -0,0 +1,12 @@
+// Copyright (c) 2017 Doyub Kim
+//
+// I am making my contributions/submissions to this project solely in my
+// personal capacity and am not conveying any rights to any intellectual
+// property of any third parties.
+
+namespace jet {
+
+void testCuda();
+
+}  // namespace jet
+
---
+++ src/tests/unit_tests/cuda_test_tests.cpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2017 Doyub Kim
+//
+// I am making my contributions/submissions to this project solely in my
+// personal capacity and am not conveying any rights to any intellectual
+// property of any third parties.
+
+#include <jet/cuda_test.h>
+
+#include <gtest/gtest.h>
+
+using namespace jet;
+
+TEST(CudaTest, TestCuda) {
+    testCuda();
+}
