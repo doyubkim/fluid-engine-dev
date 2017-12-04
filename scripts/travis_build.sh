7766e702478a4b6ada1306fb1007185f35777777
Doyub Kim <doyubkim@gmail.com>
2017-12-04 10:18:50 -0800

    Disabling pip test for macOS

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -7,7 +7,11 @@ cd build
 cmake ..
 make
 bin/unit_tests
+if [[ "$unamestr" == 'Darwin' ]]; then
+    echo "Disabling pip test for macOS"
+else
 cd ..
-pip install --user -r requirements.txt
-pip install --user .
-python src/tests/python_tests/main.py
+    pip install --user -r requirements.txt
+    pip install --user .
+    python src/tests/python_tests/main.py
+fi
