968e7e57612d7afdc5475fe9771ca017f076cf4b
Doyub Kim <doyubkim@gmail.com>
2017-12-11 06:52:57 -0800

    Fix smoke density not getting diffused (#135)
    This revision fixes the issue where smoke density diffusion is not applied for both 2D and 3D grid-based smoke solvers.

--- src/jet/grid_smoke_solver2.cpp
+++ src/jet/grid_smoke_solver2.cpp
@@ -5,20 +5,20 @@
 // property of any third parties.
 
 #include <pch.h>
+
 #include <jet/grid_smoke_solver2.h>
+
 #include <algorithm>
 
 using namespace jet;
 
 GridSmokeSolver2::GridSmokeSolver2()
-: GridSmokeSolver2({1, 1}, {1, 1}, {0, 0}) {
-}
+    : GridSmokeSolver2({1, 1}, {1, 1}, {0, 0}) {}
 
-GridSmokeSolver2::GridSmokeSolver2(
-    const Size2& resolution,
-    const Vector2D& gridSpacing,
-    const Vector2D& gridOrigin)
-: GridFluidSolver2(resolution, gridSpacing, gridOrigin) {
+GridSmokeSolver2::GridSmokeSolver2(const Size2& resolution,
+                                   const Vector2D& gridSpacing,
+                                   const Vector2D& gridOrigin)
+    : GridFluidSolver2(resolution, gridSpacing, gridOrigin) {
     auto grids = gridSystemData();
     _smokeDensityDataId = grids->addAdvectableScalarData(
         std::make_shared<CellCenteredScalarGrid2::Builder>(), 0.0);
@@ -26,8 +26,7 @@ GridSmokeSolver2::GridSmokeSolver2(
         std::make_shared<CellCenteredScalarGrid2::Builder>(), 0.0);
 }
 
-GridSmokeSolver2::~GridSmokeSolver2() {
-}
+GridSmokeSolver2::~GridSmokeSolver2() {}
 
 double GridSmokeSolver2::smokeDiffusionCoefficient() const {
     return _smokeDiffusionCoefficient;
@@ -61,9 +60,7 @@ void GridSmokeSolver2::setBuoyancyTemperatureFactor(double newValue) {
     _buoyancyTemperatureFactor = newValue;
 }
 
-double GridSmokeSolver2::smokeDecayFactor() const {
-    return _smokeDecayFactor;
-}
+double GridSmokeSolver2::smokeDecayFactor() const { return _smokeDecayFactor; }
 
 void GridSmokeSolver2::setSmokeDecayFactor(double newValue) {
     _smokeDecayFactor = clamp(newValue, 0.0, 1.0);
@@ -100,12 +97,9 @@ void GridSmokeSolver2::computeDiffusion(double timeIntervalInSeconds) {
             auto den0 = std::dynamic_pointer_cast<CellCenteredScalarGrid2>(
                 den->clone());
 
-            diffusionSolver()->solve(
-                *den0,
-                _smokeDiffusionCoefficient,
-                timeIntervalInSeconds,
-                den0.get(),
-                *colliderSdf());
+            diffusionSolver()->solve(*den0, _smokeDiffusionCoefficient,
+                                     timeIntervalInSeconds, den.get(),
+                                     *colliderSdf());
             extrapolateIntoCollider(den.get());
         }
 
@@ -114,26 +108,20 @@ void GridSmokeSolver2::computeDiffusion(double timeIntervalInSeconds) {
             auto temp0 = std::dynamic_pointer_cast<CellCenteredScalarGrid2>(
                 temp->clone());
 
-            diffusionSolver()->solve(
-                *temp0,
-                _temperatureDiffusionCoefficient,
-                timeIntervalInSeconds,
-                temp.get(),
-                *colliderSdf());
+            diffusionSolver()->solve(*temp0, _temperatureDiffusionCoefficient,
+                                     timeIntervalInSeconds, temp.get(),
+                                     *colliderSdf());
             extrapolateIntoCollider(temp.get());
         }
     }
 
     auto den = smokeDensity();
     den->parallelForEachDataPointIndex(
-        [&](size_t i, size_t j) {
-            (*den)(i, j) *= 1.0 - _smokeDecayFactor;
-        });
+        [&](size_t i, size_t j) { (*den)(i, j) *= 1.0 - _smokeDecayFactor; });
     auto temp = temperature();
-    temp->parallelForEachDataPointIndex(
-        [&](size_t i, size_t j) {
-            (*temp)(i, j) *= 1.0 - _temperatureDecayFactor;
-        });
+    temp->parallelForEachDataPointIndex([&](size_t i, size_t j) {
+        (*temp)(i, j) *= 1.0 - _temperatureDecayFactor;
+    });
 }
 
 void GridSmokeSolver2::computeBuoyancyForce(double timeIntervalInSeconds) {
@@ -151,11 +139,10 @@ void GridSmokeSolver2::computeBuoyancyForce(double timeIntervalInSeconds) {
         auto temp = temperature();
 
         double tAmb = 0.0;
-        temp->forEachCellIndex([&](size_t i, size_t j) {
-            tAmb += (*temp)(i, j);
-        });
-        tAmb /= static_cast<double>(
-            temp->resolution().x * temp->resolution().y);
+        temp->forEachCellIndex(
+            [&](size_t i, size_t j) { tAmb += (*temp)(i, j); });
+        tAmb /=
+            static_cast<double>(temp->resolution().x * temp->resolution().y);
 
         auto u = vel->uAccessor();
         auto v = vel->vAccessor();
@@ -165,9 +152,9 @@ void GridSmokeSolver2::computeBuoyancyForce(double timeIntervalInSeconds) {
         if (std::abs(up.x) > kEpsilonD) {
             vel->parallelForEachUIndex([&](size_t i, size_t j) {
                 Vector2D pt = uPos(i, j);
-                double fBuoy
-                    = _buoyancySmokeDensityFactor * den->sample(pt)
-                    + _buoyancyTemperatureFactor * (temp->sample(pt) - tAmb);
+                double fBuoy =
+                    _buoyancySmokeDensityFactor * den->sample(pt) +
+                    _buoyancyTemperatureFactor * (temp->sample(pt) - tAmb);
                 u(i, j) += timeIntervalInSeconds * fBuoy * up.x;
             });
         }
@@ -175,9 +162,9 @@ void GridSmokeSolver2::computeBuoyancyForce(double timeIntervalInSeconds) {
         if (std::abs(up.y) > kEpsilonD) {
             vel->parallelForEachVIndex([&](size_t i, size_t j) {
                 Vector2D pt = vPos(i, j);
-                double fBuoy
-                    = _buoyancySmokeDensityFactor * den->sample(pt)
-                    + _buoyancyTemperatureFactor * (temp->sample(pt) - tAmb);
+                double fBuoy =
+                    _buoyancySmokeDensityFactor * den->sample(pt) +
+                    _buoyancyTemperatureFactor * (temp->sample(pt) - tAmb);
                 v(i, j) += timeIntervalInSeconds * fBuoy * up.y;
             });
         }
@@ -186,10 +173,7 @@ void GridSmokeSolver2::computeBuoyancyForce(double timeIntervalInSeconds) {
     }
 }
 
-GridSmokeSolver2::Builder GridSmokeSolver2::builder() {
-    return Builder();
-}
-
+GridSmokeSolver2::Builder GridSmokeSolver2::builder() { return Builder(); }
 
 GridSmokeSolver2 GridSmokeSolver2::Builder::build() const {
     return GridSmokeSolver2(_resolution, getGridSpacing(), _gridOrigin);
@@ -197,11 +181,6 @@ GridSmokeSolver2 GridSmokeSolver2::Builder::build() const {
 
 GridSmokeSolver2Ptr GridSmokeSolver2::Builder::makeShared() const {
     return std::shared_ptr<GridSmokeSolver2>(
-        new GridSmokeSolver2(
-            _resolution,
-            getGridSpacing(),
-            _gridOrigin),
-        [] (GridSmokeSolver2* obj) {
-            delete obj;
-        });
+        new GridSmokeSolver2(_resolution, getGridSpacing(), _gridOrigin),
+        [](GridSmokeSolver2* obj) { delete obj; });
 }
