91e9c763b3589754575c2455216d966ff463b081
Doyub Kim <doyubkim@gmail.com>
2017-12-15 23:20:06 -0800

    [ci skip] [WIP] More comments

--- include/jet.viz/event.h
+++ include/jet.viz/event.h
@@ -12,19 +12,48 @@
 
 namespace jet {
 namespace viz {
+
+//! Event token (handle) type.
 typedef size_t EventToken;
 
+//! Empty event token.
 static const EventToken kEmptyEventToken = 0;
 
+//!
+//! \brief Generic event type.
+//!
+//! This class represents a generic event source that can send signals to
+//! multiple callback functions.
+//!
 template <typename... EventArgTypes>
 class Event {
  public:
+    //! \brief Callback function type.
+    //! The callback function type that can be attached to the event source.
+    //! Returns true if an event is handled.
     typedef std::function<bool(EventArgTypes...)> CallbackType;
 
+    //!
+    //! Invokes callback functions with given arguments.
+    //!
+    //! \param args Arguments to pass to the attached callback functions.
+    //! \return True if one ore more callback functions handled the event.
+    //!
     bool operator()(EventArgTypes... args);
 
+    //!
+    //! Attaches given callback function.
+    //!
+    //! \param callback Callback function to attach.
+    //! \return Event token (handle) for the attached callback.
+    //!
     EventToken operator+=(const CallbackType& callback);
 
+    //!
+    //! Detaches the callback function.
+    //!
+    //! \param token Event token for the callback function to detach.
+    //!
     void operator-=(EventToken token);
 
  private:
