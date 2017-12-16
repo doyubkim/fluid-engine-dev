ebb912e10086471ce0efd58fa7b7fee7db6b58ad
Doyub Kim <doyubkim@gmail.com>
2017-12-16 13:41:43 -0800

    [ci skip] [WIP] CUDA builds with Thrust on Win

---
+++ include/jet/cuda_array1.h
@@ -0,0 +1,20 @@
+// Copyright (c) 2017 Doyub Kim
+//
+// I am making my contributions/submissions to this project solely in my
+// personal capacity and am not conveying any rights to any intellectual
+// property of any third parties.
+
+#ifdef JET_USE_CUDA
+
+#include <thrust/device_vector.h>
+
+namespace jet {
+
+template <typename T>
+class CudaArray1 {
+ public:
+};
+
+}  // namespace jet
+
+#endif  // JET_USE_CUDA
---
+++ src/tests/unit_tests/cuda_array1_tests.cu
@@ -0,0 +1,8 @@
+// Copyright (c) 2017 Doyub Kim
+//
+// I am making my contributions/submissions to this project solely in my
+// personal capacity and am not conveying any rights to any intellectual
+// property of any third parties.
+
+
+#include <jet/cuda_array1.h>
