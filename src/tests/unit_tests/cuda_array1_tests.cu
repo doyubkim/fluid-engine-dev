2e1db8d3d538e2f9fce71d397587031533826dd9
Doyub Kim <doyubkim@gmail.com>
2017-12-17 00:19:47 -0800

    [ci skip] [WIP] More Thrust tests

--- src/tests/unit_tests/cuda_array1_tests.cu
+++ src/tests/unit_tests/cuda_array1_tests.cu
@@ -6,3 +6,22 @@
 
 
 #include <jet/cuda_array1.h>
+
+#include <gtest/gtest.h>
+
+using namespace jet;
+using namespace experimental;
+
+TEST(CudaArray1, Constructors) {
+    {
+        CudaArray1<float> arr;
+        EXPECT_EQ(0u, arr.size());
+    }
+    {
+        CudaArray1<float> arr(9, 1.5f);
+        EXPECT_EQ(9u, arr.size());
+        for (size_t i = 0; i < 9; ++i) {
+            EXPECT_FLOAT_EQ(1.5f, arr[i]);
+        }
+    }
+}
