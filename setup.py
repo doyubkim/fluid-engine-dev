a2de1587e8ceedbee28b53e4d3a4fe0c05be7e40
Doyub Kim <doyubkim@gmail.com>
2017-12-28 01:13:16 -0800

    [ci skip] Bumping up the version

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pyjet',
-    version='1.2.1',
+    version='1.2.2',
     author='Doyub Kim',
     author_email='doyubkim@gmail.com',
     description='Fluid simulation engine for computer graphics applications',
