ab0c7e7952507140835063d7a7c19647e53d78d4
Doyub Kim <doyubkim@gmail.com>
2018-02-17 23:31:14 -0800

    [ci skip] [WIP] adding comments to points renderables

--- include/jet.viz/points_renderable2.h
+++ include/jet.viz/points_renderable2.h
@@ -11,26 +11,45 @@
 #include <jet.viz/renderable.h>
 #include <jet.viz/shader.h>
 #include <jet.viz/vertex_buffer.h>
+#include <jet/array_view1.h>
 
 namespace jet {
 namespace viz {
 
+//! 2-D points renderable.
 class PointsRenderable2 final : public Renderable {
  public:
+    //! Constructs a renderable.
     PointsRenderable2(Renderer* renderer);
 
+    //! Destructor.
     virtual ~PointsRenderable2();
 
+    //! Sets the positions of the points.
     void setPositions(const Vector2F* positions, size_t numberOfVertices);
 
+    //! Sets the positions of the points.
+    void setPositions(ConstArrayView1<Vector2F> positions);
+
+    //! Sets the positions and colors of the points.
     void setPositionsAndColors(const Vector2F* positions, const Color* colors,
                                size_t numberOfVertices);
 
+    //! Sets the positions and colors of the points.
+    void setPositionsAndColors(ConstArrayView1<Vector2F> positions,
+                               ConstArrayView1<Color> colors);
+
+    //! Returns vertex buffer object.
+    VertexBuffer* vertexBuffer() const;
+
+    //! Returns radius of the points.
     float radius() const;
 
+    //! Sets the radius of the points.
     void setRadius(float radius);
 
  protected:
+    //! Renders points in 2-D space.
     virtual void render(Renderer* renderer) override;
 
  private:
@@ -42,6 +61,7 @@ class PointsRenderable2 final : public Renderable {
     void updateVertexBuffer(const std::vector<VertexPosition3Color4>& vertices);
 };
 
+//! Shared pointer type for PointsRenderable2.
 typedef std::shared_ptr<PointsRenderable2> PointsRenderable2Ptr;
 
 }  // namespace viz
