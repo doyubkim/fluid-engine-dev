265b4fe821e75340adb28938861c73c4a18fad3d
Doyub Kim <doyubkim@gmail.com>
2018-02-22 22:48:19 -0800

    [ci skip] [WIP] Adding comments for render parameters

--- include/jet.viz/primitive_types.h
+++ include/jet.viz/primitive_types.h
@@ -16,10 +16,19 @@ namespace viz {
 
 //! Render primitive types
 enum class PrimitiveType {
+    //! Multiple points primitive.
     Points = 0,
+
+    //! Multiple lines primitive.
     Lines,
+
+    //! Line strip primitive.
     LineStrip,
+
+    //! Multiple triangles primitive.
     Triangles,
+
+    //! Triangle strip primitive.
     TriangleStrip,
 };
 
--- include/jet.viz/render_parameters.h
+++ include/jet.viz/render_parameters.h
@@ -14,10 +14,13 @@
 #include <vector>
 
 namespace jet {
+
 namespace viz {
 
+//! Rendering parameters set.
 class RenderParameters final {
  public:
+    //! Rendering parameter types.
     enum class Type {
         kInt,
         kUInt,
@@ -28,49 +31,70 @@ class RenderParameters final {
         kMatrix,
     };
 
+    //! Rendering parameter metadata.
     struct Metadata {
         size_t offset;
         Type type;
     };
 
+    //! Adds a 32-bit integer parameter.
     void add(const std::string& name, int32_t defaultValue);
 
+    //! Adds a 32-bit unsigned integer parameter.
     void add(const std::string& name, uint32_t defaultValue);
 
+    //! Adds a 32-bit float parameter.
     void add(const std::string& name, float defaultValue);
 
+    //! Adds a 32-bit 2-D float parameter.
     void add(const std::string& name, const Vector2F& defaultValue);
 
+    //! Adds a 32-bit 3-D float parameter.
     void add(const std::string& name, const Vector3F& defaultValue);
 
+    //! Adds a 32-bit 4-D float parameter.
     void add(const std::string& name, const Vector4F& defaultValue);
 
+    //! Adds a 32-bit 4x4 float matrix parameter.
     void add(const std::string& name, const Matrix4x4F& defaultValue);
 
+    //! Sets a 32-bit integer parameter.
     void set(const std::string& name, int32_t value);
 
+    //! Sets a 32-bit unsigned integer parameter.
     void set(const std::string& name, uint32_t value);
 
+    //! Sets a 32-bit float parameter.
     void set(const std::string& name, float value);
 
+    //! Sets a 32-bit 2-D float parameter.
     void set(const std::string& name, const Vector2F& value);
 
+    //! Sets a 32-bit 3-D float parameter.
     void set(const std::string& name, const Vector3F& value);
 
+    //! Sets a 32-bit 4-D float parameter.
     void set(const std::string& name, const Vector4F& value);
 
+    //! Sets a 32-bit 4x4 float matrix parameter.
     void set(const std::string& name, const Matrix4x4F& value);
 
+    //! Returns true if a parameter exists in the set with \p name.
     bool has(const std::string& name) const;
 
+    //! Returns every name of the parameters.
     const std::vector<std::string>& names() const;
 
+    //! Returns raw pointer to the buffer.
     const int32_t* buffer() const;
 
+    //! Returns raw pointer to the parameter in the buffer.
     const int32_t* buffer(const std::string& name) const;
 
+    //! Returns size of the buffer in bytes.
     size_t bufferSizeInBytes() const;
 
+    //! Returns metadata of the parameter with \p name.
     Metadata metadata(const std::string& name) const;
 
  private:
@@ -86,6 +110,7 @@ class RenderParameters final {
 };
 
 }  // namespace viz
+
 }  // namespace jet
 
 #endif  // INCLUDE_JET_VIZ_RENDER_PARAMETERS_H_
