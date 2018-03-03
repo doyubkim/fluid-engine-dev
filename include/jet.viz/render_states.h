c80799e3baa70f808672a6efa9ea0976101a8c25
Doyub Kim <doyubkim@gmail.com>
2018-03-02 16:02:57 -0800

    [ci skip] [WIP] More comments

--- include/jet.viz/render_states.h
+++ include/jet.viz/render_states.h
@@ -7,36 +7,76 @@
 #ifndef INCLUDE_JET_VIZ_RENDER_STATES_H_
 #define INCLUDE_JET_VIZ_RENDER_STATES_H_
 
-namespace jet { namespace viz {
+namespace jet {
+namespace viz {
 
+//! Rendering state representation.
 struct RenderStates final {
+    //! Cull modes.
     enum class CullMode {
+        //! No culling.
         None = 0,
+
+        //! Front-face culling.
         Front,
+
+        //! Back-face culling.
         Back,
     };
 
+    //! Alpha-blending factors.
     enum class BlendFactor {
+        //! Use 0 blend factor.
         Zero = 0,
+
+        //! Use 1 blend factor.
         One,
+
+        //! Use source color blend factor.
         SrcColor,
+
+        //! Use 1 - source color blend factor.
         OneMinusSrcColor,
+
+        //! Use source alpha blend factor.
         SrcAlpha,
+
+        //! Use 1 - source alpha blend factor.
         OneMinusSrcAlpha,
+
+        //! Use destination alpha blend factor.
         DestAlpha,
+
+        //! Use 1 - destination alpha blend factor.
         OneMinusDestAlpha,
+
+        //! Use destination color blend factor.
         DestColor,
+
+        //! Use 1 - destination color blend factor.
         OneMinusDestColor,
     };
 
+    //! True if front face is defined as clock-wise order.
     bool isFrontFaceClockWise = true;
+
+    //! True if blending is enabled.
     bool isBlendEnabled = true;
+
+    //! True if depth test is enabled.
     bool isDepthTestEnabled = true;
+
+    //! The cull mode.
     CullMode cullMode = CullMode::Back;
+
+    //! The blend factor for the source.
     BlendFactor sourceBlendFactor = BlendFactor::SrcAlpha;
+
+    //! The blend factor for the destination.
     BlendFactor destinationBlendFactor = BlendFactor::OneMinusSrcAlpha;
 };
 
-} }  // namespace jet::viz
+}  // namespace viz
+}  // namespace jet
 
 #endif  // INCLUDE_JET_VIZ_RENDER_STATES_H_
--- include/jet.viz/renderer.h
+++ include/jet.viz/renderer.h
@@ -26,71 +26,186 @@
 #include <vector>
 
 namespace jet {
+
 namespace viz {
 
+//!
+//! \brief Abstract base class for renderer.
+//!
+//! This class defines the interface for the renderers. Actual renderers should
+//! override purely virtual functions in order to implement its own rendering
+//! backend (such as OpenGL, Metal, Vulkan, D3D, etc).
+//!
 class Renderer {
  public:
+    //! Default constructor.
     Renderer();
+
+    //! Default destructor.
     virtual ~Renderer();
 
+    //!
+    //! Creates a vertex buffer with given parameters.
+    //!
+    //! \param shader Shader object for the buffer.
+    //! \param vertices Vertex data.
+    //! \param numberOfPoints Number of vertices.
+    //! \return New vertex buffer.
+    //!
     virtual VertexBufferPtr createVertexBuffer(const ShaderPtr& shader,
                                                const float* vertices,
                                                size_t numberOfPoints) = 0;
 
+    //!
+    //! Creates an index buffer with given parameters.
+    //!
+    //! \param vertexBuffer Vertices for the index buffer.
+    //! \param indices Index data.
+    //! \param numberOfIndices Number of indices.
+    //! \return New index buffer.
+    //!
     virtual IndexBufferPtr createIndexBuffer(
         const VertexBufferPtr& vertexBuffer, const uint32_t* indices,
         size_t numberOfIndices) = 0;
 
+    //!
+    //! Creates a 2-D texture with 8-bit image and given parameters.
+    //!
+    //! \param data 8-bit texture image data.
+    //! \param size Size of the data.
+    //! \return New 2-D texture.
+    //!
     virtual Texture2Ptr createTexture2(
         const ConstArrayAccessor2<ByteColor>& data) = 0;
 
+    //!
+    //! Creates a 2-D texture with 32-bit image and given parameters.
+    //!
+    //! \param data 32-bit texture image data.
+    //! \param size Size of the data.
+    //! \return New 2-D texture.
+    //!
     virtual Texture2Ptr createTexture2(
         const ConstArrayAccessor2<Color>& data) = 0;
 
+    //!
+    //! Creates a 3-D texture with 8-bit image and given parameters.
+    //!
+    //! \param data 8-bit texture image data.
+    //! \param size Size of the data.
+    //! \return New 3-D texture.
+    //!
     virtual Texture3Ptr createTexture3(
-            const ConstArrayAccessor3<ByteColor>& data) = 0;
+        const ConstArrayAccessor3<ByteColor>& data) = 0;
 
+    //!
+    //! Creates a 3-D texture with 32-bit image and given parameters.
+    //!
+    //! \param data 32-bit texture image data.
+    //! \param size Size of the data.
+    //! \return New 3-D texture.
+    //!
     virtual Texture3Ptr createTexture3(
         const ConstArrayAccessor3<Color>& data) = 0;
 
+    //!
+    //! Creates a shader object with given preset shader name.
+    //!
+    //! \param shaderName Preset shader name.
+    //! \return New shader.
+    //!
     virtual ShaderPtr createPresetShader(
         const std::string& shaderName) const = 0;
 
+    //!
+    //! Sets current render primitive type state for drawing.
+    //!
+    //! \param type Primitive type.
+    //!
     virtual void setPrimitiveType(PrimitiveType type) = 0;
 
+    //!
+    //! Draws currently bound object.
+    //!
+    //! \param numberOfVertices Number of vertices.
+    //!
     virtual void draw(size_t numberOfVertices) = 0;
 
+    //!
+    //! Draws currently bound indexed object.
+    //!
+    //! \param numberOfIndices Number of indices.
+    //!
     virtual void drawIndexed(size_t numberOfIndices) = 0;
 
+    //! Renders a frame.
     void render();
+
+    //! Resizes the viewport.
     void resize(const Viewport& viewport);
+
+    //! Adds a renderable to the renderer.
     void addRenderable(const RenderablePtr& renderable);
+
+    //! Clears all renderables.
     void clearRenderables();
 
+    //! Binds a shader to the current rendering context.
     void bindShader(const ShaderPtr& shader);
+
+    //! Unbinds a shader to the current rendering context.
     void unbindShader(const ShaderPtr& shader);
+
+    //! Binds a vertex buffer to the current rendering context.
     void bindVertexBuffer(const VertexBufferPtr& vertexBuffer);
+
+    //! Unbinds a vertex to the current rendering context.
     void unbindVertexBuffer(const VertexBufferPtr& vertexBuffer);
+
+    //! Binds a index buffer to the current rendering context.
     void bindIndexBuffer(const IndexBufferPtr& indexBuffer);
+
+    //! Unbinds a index buffer to the current rendering context.
     void unbindIndexBuffer(const IndexBufferPtr& indexBuffer);
+
+    //! Binds a 2-D texture to the current rendering context.
     void bindTexture(const Texture2Ptr& texture, unsigned int slotId);
+
+    //! Binds a 3-D texture to the current rendering context.
     void bindTexture(const Texture3Ptr& texture, unsigned int slotId);
 
+    //! Returns current viewport.
     const Viewport& viewport() const;
 
+    //! Returns current camera.
     const CameraPtr& camera() const;
+
+    //! Sets camera.
     void setCamera(const CameraPtr& camera);
 
+    //! Returns current render states.
     const RenderStates& renderStates() const;
+
+    //! Sets current render states.
     void setRenderStates(const RenderStates& states);
 
+    //! Returns current background color.
     const Color& backgroundColor() const;
+
+    //! Sets current background color.
     void setBackgroundColor(const Color& color);
 
  protected:
+    //! Called when rendering a frame begins.
     virtual void onRenderBegin() = 0;
+
+    //! Called when rendering a frame ended.
     virtual void onRenderEnd() = 0;
+
+    //! Called when the view has resized.
     virtual void onResize(const Viewport& viewport) = 0;
+
+    //! Called when the render states has changed.
     virtual void onSetRenderStates(const RenderStates& states) = 0;
 
  private:
@@ -101,9 +216,11 @@ class Renderer {
     std::vector<RenderablePtr> _renderables;
 };
 
+//!
 typedef std::shared_ptr<Renderer> RendererPtr;
 
 }  // namespace viz
+
 }  // namespace jet
 
 #endif  // INCLUDE_JET_VIZ_RENDERER_H_
