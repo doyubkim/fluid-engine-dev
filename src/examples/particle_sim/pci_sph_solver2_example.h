72da750090726dac27011b1b8d90614941794f75
Doyub Kim <doyubkim@gmail.com>
2018-03-12 16:49:59 -0700

    [ci skip] [WIP] Better example GUI

--- src/examples/particle_sim/pci_sph_solver2_example.h
+++ src/examples/particle_sim/pci_sph_solver2_example.h
@@ -12,6 +12,7 @@
 #include <jet.viz/points_renderable2.h>
 #include <jet/pci_sph_solver2.h>
 
+#include <atomic>
 #include <mutex>
 
 class PciSphSolver2Example final : public Example {
@@ -20,6 +21,8 @@ class PciSphSolver2Example final : public Example {
 
     ~PciSphSolver2Example();
 
+    std::string name() const override;
+
  private:
     jet::PciSphSolver2Ptr _solver;
     jet::viz::PointsRenderable2Ptr _renderable;
@@ -27,6 +30,13 @@ class PciSphSolver2Example final : public Example {
 
     bool _areVerticesDirty = false;
     std::mutex _verticesMutex;
+
+    std::atomic<double> _viscosityCoefficient;
+    std::atomic<double> _pseudoViscosityCoefficient;
+    std::atomic<double> _maxDensityErrorRatio;
+    std::atomic<unsigned int> _maxNumberOfIterations;
+
+    void onRestartSim() override;
 
 #ifdef JET_USE_GL
     void onSetup(jet::viz::GlfwWindow* window) override;
