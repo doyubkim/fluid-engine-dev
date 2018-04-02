05de0da79f28ef6b54fb5c7f995656d0459d6df4
Doyub Kim <doyubkim@gmail.com>
2018-04-02 00:34:14 -0700

    [ci skip] [WIP] Minor doc addition

--- include/jet.viz/view_controller.h
+++ include/jet.viz/view_controller.h
@@ -7,44 +7,79 @@
 #ifndef INCLUDE_JET_VIZ_VIEW_CONTROLLER_H_
 #define INCLUDE_JET_VIZ_VIEW_CONTROLLER_H_
 
-#include <jet.viz/event.h>
 #include <jet.viz/camera.h>
+#include <jet.viz/event.h>
 #include <jet.viz/input_events.h>
 
-namespace jet { namespace viz {
+namespace jet {
 
+namespace viz {
+
+//! Abstract base class for view controllers.
 class ViewController {
  public:
+    //! Constructs a view controller with gievn camera.
     ViewController(const CameraPtr& newCamera);
+
+    //! Default destructor.
     virtual ~ViewController();
 
+    //! Called when key-down event is raised.
     void keyDown(const KeyEvent& keyEvent);
+
+    //! Called when key-up event is raised.
     void keyUp(const KeyEvent& keyEvent);
 
+    //! Called when pointer-pressed event is raised.
     void pointerPressed(const PointerEvent& pointerEvent);
+
+    //! Called when pointer-hover event is raised.
     void pointerHover(const PointerEvent& pointerEvent);
+
+    //! Called when pointer-dragged event is raised.
     void pointerDragged(const PointerEvent& pointerEvent);
+
+    //! Called when pointer released event is raised.
     void pointerReleased(const PointerEvent& pointerEvent);
+
+    //! Called when mouse-wheel event is raised.
     void mouseWheel(const PointerEvent& pointerEvent);
 
+    //! Called when view is resized.
     void resize(const Viewport& viewport);
 
+    //! Returns camera pointer.
     const CameraPtr& camera() const;
 
+    //! Returns BasicCameraState changed event object.
     Event<ViewController*>& onBasicCameraStateChanged();
 
  protected:
+    //! Called when key-down event is handled.
     virtual void onKeyDown(const KeyEvent& keyEvent);
+
+    //! Called when key-up event is handled.
     virtual void onKeyUp(const KeyEvent& keyEvent);
 
+    //! Called when pointer-pressed event is handled.
     virtual void onPointerPressed(const PointerEvent& pointerEvent);
+
+    //! Called when pointer-hover event is handled.
     virtual void onPointerHover(const PointerEvent& pointerEvent);
+
+    //! Called when pointer-dragged event is handled.
     virtual void onPointerDragged(const PointerEvent& pointerEvent);
+
+    //! Called when pointer-released event is handled.
     virtual void onPointerReleased(const PointerEvent& pointerEvent);
+
+    //! Called when mouse-wheel event is handled.
     virtual void onMouseWheel(const PointerEvent& pointerEvent);
 
+    //! Called when resize event is handled.
     virtual void onResize(const Viewport& viewport);
 
+    //! Sets the basic camera state of the current camera.
     void setBasicCameraState(const BasicCameraState& newState);
 
  private:
@@ -52,8 +87,11 @@ class ViewController {
     CameraPtr _camera;
 };
 
+//! Shared pointer type for ViewController.
 typedef std::shared_ptr<ViewController> ViewControllerPtr;
 
-} }  // namespace jet::viz
+}  // namespace viz
+
+}  // namespace jet
 
 #endif  // INCLUDE_JET_VIZ_VIEW_CONTROLLER_H_
