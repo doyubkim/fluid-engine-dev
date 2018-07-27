9753cd61981cc5d8b77074f144ad4529904a05b4
Doyub Kim <doyubkim@gmail.com>
2018-07-27 00:37:06 -0700

    Codecov integration

---
+++ scripts/travis_build_codecov.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -e
+
+export NUM_JOBS=1
+
+mkdir build
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Debug
+make -j 8
+lcov -c -i -d src/tests/unit_tests -o base.info
+bin/unit_tests
+lcov -c -d src/tests/unit_tests -o test.info
+lcov -a base.info -a test.info -o coverage.info
+lcov -r coverage.info '/usr/*' -o coverage.info
+lcov -r coverage.info '*/external/*' -o coverage.info
+lcov -r coverage.info '*/src/tests/*' -o coverage.info
+lcov -l coverage.info
+bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
