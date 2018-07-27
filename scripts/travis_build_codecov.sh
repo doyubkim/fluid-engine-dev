a57a89436af444cb30cef9687aae85f5ace5d77f
Doyub Kim <doyubkim@gmail.com>
2018-07-27 01:18:20 -0700

    A bit more tweaking

--- scripts/travis_build_codecov.sh
+++ scripts/travis_build_codecov.sh
@@ -9,7 +9,7 @@ sudo apt-get install -yq lcov
 mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
-make -j 8
+make -j 8 unit_tests
 lcov -c -i -d src/tests/unit_tests -o base.info
 bin/unit_tests
 lcov -c -d src/tests/unit_tests -o test.info
@@ -18,4 +18,3 @@ lcov -r coverage.info '/usr/*' -o coverage.info
 lcov -r coverage.info '*/external/*' -o coverage.info
 lcov -r coverage.info '*/src/tests/*' -o coverage.info
 lcov -l coverage.info
-bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
