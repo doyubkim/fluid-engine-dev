5e57f4c596e349412f0e15d2a94857317d431691
Doyub Kim <doyubkim@gmail.com>
2021-04-24 23:39:29 -0700

    Make timer test more relaxed

--- src/tests/unit_tests/timer_tests.cpp
+++ src/tests/unit_tests/timer_tests.cpp
@@ -14,8 +14,8 @@ using namespace jet;
 
 TEST(Timer, Basics) {
     Timer timer, timer2;
-    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-    EXPECT_LT(0.01, timer.durationInSeconds());
+    std::this_thread::sleep_for(std::chrono::milliseconds(101));
+    EXPECT_LT(0.1, timer.durationInSeconds());
 
     timer.reset();
     EXPECT_LE(timer.durationInSeconds(), timer2.durationInSeconds());
